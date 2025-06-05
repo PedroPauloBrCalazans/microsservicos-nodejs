f9d60ecbd2baef172d83bca4e127361df34ef60c
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-06-05 00:30:59 -0300

    feat: Ajustes de configuração base + Add tsconfig

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -9,6 +9,33 @@ import {
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+//toda vez que criamos uma aplicação com Escalonamento horizontal ou DEPLOY: Blue-green deployment precisamos de uma rota de health check -> verifica se a aplicação está funcionando, verifica se o serviço do NODE está no ar e respondendo em tempo
+
+app.get("/health", () => {
+  return "ok";
+});
+
+app.post(
+  "/orders",
+  {
+    schema: {
+      body: z.object({
+        amount: z.number(),
+      }),
+    },
+  },
+  (request, response) => {
+    const { amount } = request.body;
+
+    console.log("Create an order with amount", amount);
+
+    return response.status(201).send();
+  }
+);
+
 app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
   console.log("[Orders] HTTP Server running!");
 });
