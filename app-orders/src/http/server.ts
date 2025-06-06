337dc6c84361c9d5f9d8c2d95a01507e7df38a1a
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-06-06 01:38:10 -0300

    feat: Configuração do broker com rabbitmq + ajustes no tsconfig + add conexão com BD

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import { randomUUID } from "node:crypto";
 import { z } from "zod";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import { channels } from "../broker/channels/index.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -13,6 +15,8 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 //toda vez que criamos uma aplicação com Escalonamento horizontal ou DEPLOY: Blue-green deployment precisamos de uma rota de health check -> verifica se a aplicação está funcionando, verifica se o serviço do NODE está no ar e respondendo em tempo
+
+app.register(fastifyCors, { origin: "*" });
 
 app.get("/health", () => {
   return "ok";
@@ -23,7 +27,7 @@ app.post(
   {
     schema: {
       body: z.object({
-        amount: z.number(),
+        amount: z.coerce.number(),
       }),
     },
   },
@@ -31,6 +35,11 @@ app.post(
     const { amount } = request.body;
 
     console.log("Create an order with amount", amount);
+
+    channels.orders.sendToQueue(
+      "orders",
+      Buffer.from(JSON.stringify({ amount }))
+    );
 
     return response.status(201).send();
   }
